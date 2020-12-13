9395e5313d6a5b0ffd7641acd5e4f63b2163c876
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2020-12-13 10:59:10 +0100

    Speed compare for Array added

--- sandbox/disk.pl
+++ sandbox/disk.pl
@@ -3,7 +3,7 @@
 use 5.14.2;
 use warnings;
 
-our $VERSION = "0.11";
+our $VERSION = "0.12";
 our $CMD = $0 =~ s{.*/}{}r;
 
 sub usage {
@@ -17,6 +17,7 @@ GetOptions (
     "help|?"		=> sub { usage (0); },
     "V|version"		=> sub { say "$CMD [$VERSION]"; exit 0; },
 
+    "a|array!"		=> \ my $opt_a,
     "f|s|fast|short!"	=> \ my $opt_f,
     "l|long|slow!"	=> \ my $opt_l,
 
@@ -28,7 +29,10 @@ use Text::CSV_XS qw( csv );
 use DB_File;
 use CDB_File;
 use Tie::Hash::DBD;
-eval "use $_" for qw( Redis::Hash GDBM_File NDBM_File ODBM_File SDBM_File KyotoCabinet );
+use Tie::Array::DBD;
+eval "use $_" for qw( GDBM_File NDBM_File ODBM_File SDBM_File
+		      Redis::Hash Redis::Fast::Hash
+		      KyotoCabinet );
 my $DB_CREATE = eval "use BerkeleyDB; DB_CREATE;";
 
 use Time::HiRes qw( gettimeofday tv_interval );
@@ -38,26 +42,28 @@ my $u = $ENV{LOGNAME} || $ENV{USER} || getpwuid $<;
 my %t;
 
 my @conf = (
-    [ "perl",    undef,             undef				],
+    [ "perl",      undef,              undef				],
 
-    [ "GDBM",      "GDBM_File",       "db.8", O_RDWR|O_CREAT, 0666	],
-    [ "NDBM",      "NDBM_File",       "db.7", O_RDWR|O_CREAT, 0666	],
-    [ "ODBM",      "ODBM_File",       "db.6", O_RDWR|O_CREAT, 0666	],
-    [ "SDBM",      "SDBM_File",       "db.5", O_RDWR|O_CREAT, 0666	],
-    [ "DB_File",   "DB_File",         "db.2", O_RDWR|O_CREAT, 0666	],
-    [ "CDB_File",  "CDB_File",        "db.3"				],
+    [ "GDBM",      "GDBM_File",        "db.8", O_RDWR|O_CREAT, 0666	],
+    [ "NDBM",      "NDBM_File",        "db.7", O_RDWR|O_CREAT, 0666	],
+    [ "ODBM",      "ODBM_File",        "db.6", O_RDWR|O_CREAT, 0666	],
+    [ "SDBM",      "SDBM_File",        "db.5", O_RDWR|O_CREAT, 0666	],
+    [ "DB_File",   "DB_File",          "db.2", O_RDWR|O_CREAT, 0666	],
+    [ "CDB_File",  "CDB_File",         "db.3"				],
     [ "BerkeleyDB","BerkeleyDB::Hash", -Filename => "db.4",
 				       -Flags    => $DB_CREATE,		],
     [ "KyotoCab",  "KyotoCabinet::DB", "casket.kch"	],
-    [ "Redis",     "Redis::Hash",     "dbd_"				],
-    [ "Redis2",    "Redis::Hash",     "dbd2_", encoding => undef	],
-    [ "SQLite",    "Tie::Hash::DBD",  "dbi:SQLite:dbname=db.1"		],
-    [ "Pg",        "Tie::Hash::DBD",  "dbi:Pg:"				],
-    [ "mysql",     "Tie::Hash::DBD",  "dbi:mysql:database=$u;user=$u"	],
-    [ "MariaDB",   "Tie::Hash::DBD",  "dbi:MariaDB:database=$u;user=$u"	],
-    [ "CSV",       "Tie::Hash::DBD",  "dbi:CSV:f_ext=.csv/r;csv_null=1"	],
-    [ "Oracle",    "Tie::Hash::DBD",  "dbi:Oracle:"			],
-    [ "Unify",     "Tie::Hash::DBD",  "dbi:Unify:"			],
+    [ "Redis",     "Redis::Hash",      "dbd_"				],
+    [ "Redis2",    "Redis::Hash",      "dbd2_",  encoding => undef	],
+    [ "RedisFast", "Redis::Fast::Hash","dbdf_"				],
+    [ "RedisFast2","Redis::Fast::Hash","dbdf2_", encoding => undef	],
+    [ "SQLite",    "Tie::Hash::DBD",   "dbi:SQLite:dbname=db.1"		],
+    [ "Pg",        "Tie::Hash::DBD",   "dbi:Pg:"			],
+    [ "mysql",     "Tie::Hash::DBD",   "dbi:mysql:database=$u;user=$u"	],
+    [ "MariaDB",   "Tie::Hash::DBD",   "dbi:MariaDB:database=$u;user=$u"],
+    [ "CSV",       "Tie::Hash::DBD",   "dbi:CSV:f_ext=.csv/r;csv_null=1"],
+    [ "Oracle",    "Tie::Hash::DBD",   "dbi:Oracle:"			],
+    [ "Unify",     "Tie::Hash::DBD",   "dbi:Unify:"			],
     );
 
 unlink $_ for glob ("db.[0-9]*"), glob ("t_tie*.csv"), "casket.kch";
@@ -65,10 +71,14 @@ unlink $_ for glob ("db.[0-9]*"), glob ("t_tie*.csv"), "casket.kch";
 my @csv = ([qw( method direction size speed )]);
 
 foreach my $r (@conf) {
-    my ($name, $pkg, @args, %hash) = @$r;
+    my ($name, $pkg, @args, %hash, @array, $rv) = @$r;
 
     local $ENV{DBI_USER} = $ENV{DBI_USER};
     local $ENV{DBI_PASS} = $ENV{DBI_PASS};
+
+    if ($opt_a && $pkg) {
+	$pkg =~ s/::Hash::/::Array::/    or next;
+	}
 
     if ($name eq "Oracle") {
 	-d ($ENV{ORACLE_HOME} || "\x01") or next;
@@ -83,7 +93,10 @@ foreach my $r (@conf) {
 	}
 
     if ($pkg) {
-	eval { tie %hash, $pkg, @args };
+	eval { $opt_a
+	    ? tie @array, $pkg, @args
+	    : tie %hash,  $pkg, @args
+	    };
 	if ($@) {
 	    warn "$name:", $@;
 	    next;
@@ -103,25 +116,42 @@ foreach my $r (@conf) {
 
 	print STDERR " $name $size                \r";
 
-	my %plain = map { ( $_ => $_ ) }
-		    map { ( $_, pack "l", $_ ) }
-		    -($size - 1) .. $size;
+	my ($t0, $s_size);
+	if ($opt_a) {
+	    my @plain = -($size - 1) .. $size;
 
-	%hash = ();
-	my $s_size = 2 * $size;
+	    @array = ();
+	    $s_size = 2 * $size;
 
-	my $t0 = [ gettimeofday ];
-	%hash = %plain;
-	my $wv = $s_size / tv_interval ($t0);
-	$t{$s_size}{wr}{$name} = $wv;
-	push @csv, [ @tag, "wr", $s_size, $wv ];
+	    $t0 = [ gettimeofday ];
+	    @array = @plain;
+	    my $wv = $s_size / tv_interval ($t0);
+	    $t{$s_size}{wr}{$name} = $wv;
+	    push @csv, [ @tag, "wr", $s_size, $wv ];
 
-	$t0 = [ gettimeofday ];
-	my %x = %hash;
+	    $t0 = [ gettimeofday ];
+	    my @x = @array;
+	    }
+	else {
+	    my %plain = map { ( $_ => $_ ) }
+			map { ( $_, pack "l", $_ ) }
+			-($size - 1) .. $size;
+
+	    %hash = ();
+	    $s_size = 2 * $size;
+
+	    $t0 = [ gettimeofday ];
+	    %hash = %plain;
+	    my $wv = $s_size / tv_interval ($t0);
+	    $t{$s_size}{wr}{$name} = $wv;
+	    push @csv, [ @tag, "wr", $s_size, $wv ];
+
+	    $t0 = [ gettimeofday ];
+	    my %x = %hash;
+	    }
 	my $rv = $s_size / tv_interval ($t0);
 	$t{$s_size}{rd}{$name} = $rv;
 	push @csv, [ @tag, "rd", $s_size, $rv ];
-
 	$rv < 275 and last; # Next size will take too long
 	}
 
@@ -148,4 +178,4 @@ foreach my $size (sort { $a <=> $b } keys %t) {
 	}
     }
 
-csv (in => \@csv, out => "disk.csv");
+csv (in => \@csv, out => $opt_a ? "disk-a.csv" : "disk.csv");
