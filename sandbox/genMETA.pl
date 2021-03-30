3e3a9919db07f75244bed4689dd7ffe4c8f79ffc
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2021-03-30 10:39:49 +0200

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -66,7 +66,7 @@ recommends:
     DBI:                 1.643
     DBD::SQLite:         1.66
     DBD::Pg:             3.14.2
-    DBD::CSV:            0.57
+    DBD::CSV:            0.58
 test_recommends:
     Test::More:          1.302183
 resources:
